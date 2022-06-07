c8f4093c84ca404b56651245e4213c3562b87bfe
sergiuchilat <chilatsergiu@gmail.com>
2022-06-07 11:36:04 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from "react-dom/client";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from './components/Layout';
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,22 +1,24 @@
 import { Outlet, Link } from "react-router-dom";
+import { AppBar } from "@mui/material";
+import { Toolbar } from "@mui/material";
 
 const Layout = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    return (
+        <>
+            <AppBar position="sticky">
+                <Toolbar variant="dense">
+                <nav>
+                    <Link color="primary" to="/">Home</Link>
+                    <Link to="/contact">Contact</Link>
+                </nav>
+                </Toolbar>
+                
+            </AppBar>
 
-      <Outlet />
-    </>
-  )
+
+            <Outlet />
+        </>
+    )
 };
 
 export default Layout;
