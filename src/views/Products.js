30648c7617ca0fcdbe599fb51e2786d53e6964a8
sergiuchilat <chilatsergiu@gmail.com>
2022-06-07 13:49:06 +0300

    Add loader fot GET request

--- src/views/Products.js
+++ src/views/Products.js
@@ -7,29 +7,38 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import React from 'react';
 import axios from 'axios';
+import { LinearProgress } from '@mui/material';
 
 export default class Products extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts : []
+            posts: [],
+            loading: false
         }
     }
     async componentDidMount() {
-        const response  = await axios.get("https://my-json-server.typicode.com/typicode/demo/posts");
-        this.setState({posts: response.data})
-        console.log(this.state.posts);
+        this.setState({ loading: true })
+        try{
+            const response = await axios.get("https://my-json-server.typicode.com/typicode/demo/posts");
+            this.setState({ posts: response.data })
+        } catch(e){
+            console.log(e);
+        } finally{
+            this.setState({ loading: false })
+        }
     }
 
     render() {
-        return (
-            <>
+
+        if (!this.state.loading) {
+            return (
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
                                 <TableCell>ID</TableCell>
-                                <TableCell align="right">Title</TableCell>
+                                <TableCell>Title</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -44,13 +53,17 @@ export default class Products extends React.Component {
                                     <TableCell component="th" scope="row">
                                         {row.title}
                                     </TableCell>
-            
+
                                 </TableRow>
                             ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-            </>
-        )
+            )
+        } else {
+            return (
+                <LinearProgress></LinearProgress>
+            )
+        }
     }
 }
